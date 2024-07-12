2e66f0bf483e130aba78285132458d3ff31acf95
Nguyễn Phúc Khang <khang2073@gmail.com>
2024-07-12 20:47:21 +0700

    Updates
    + Add components CDataTable
    + Update Abstract page

--- src/views/Abstract.js
+++ src/views/Abstract.js
@@ -17,49 +17,34 @@ import {
   CCard,
 } from '@coreui/react'
 
-import CIcon from '@coreui/icons-react'
-// import {
-
-// } from '@coreui/icons'
-
 import styles from '../assets/css/styles.module.css'
+import CDataTable from '../views/widgets/@coreui-v3/CDataTables/CDataTable'
 
 const Abstract = () => {
+  const fields = [
+    { key: 'Submission ID', label: 'Submission ID' },
+    { key: 'Authors', label: 'Authors' },
+    { key: 'Title', label: 'Title' },
+    { key: 'Abstract', label: 'Abstract' },
+  ]
+
   return (
     <>
       <CRow className={styles.cardbody}>Abstract Book</CRow>
-      <CForm className={styles.searchbox}>
-        <CContainer>
-          <CRow>
-            <CCol>
-              <CFormInput placeholder="Search for papers" />
-            </CCol>
-            <CCol>
-              <CButton color="primary">Search</CButton>
-            </CCol>
-          </CRow>
-        </CContainer>
-      </CForm>
       <CCard>
         <CCardBody>
-          <CTable striped>
-            <CTableHead>
-              <CTableRow>
-                <CTableHeaderCell>Abstract Title</CTableHeaderCell>
-                <CTableHeaderCell>Authors</CTableHeaderCell>
-                <CTableHeaderCell>Abstract</CTableHeaderCell>
-                <CTableHeaderCell>Keywords</CTableHeaderCell>
-              </CTableRow>
-            </CTableHead>
-            <CTableBody>
-              <CTableRow>
-                <CTableDataCell>Title 1</CTableDataCell>
-                <CTableDataCell>Authors 1</CTableDataCell>
-                <CTableDataCell>Abstract 1</CTableDataCell>
-                <CTableDataCell>Keywords 1</CTableDataCell>
-              </CTableRow>
-            </CTableBody>
-          </CTable>
+          <CDataTable
+            fields={fields}
+            tableFilter
+            hover
+            sorter
+            outlined
+            border
+            responsive
+            itemsPerPageSelect
+            itemsPerPage={5} // Number of items per page
+            pagination
+          />
         </CCardBody>
       </CCard>
     </>
