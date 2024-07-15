5a1bd818082477f27019299460c1b6cf86d39fbc
Nguyễn Phúc Khang <khang2073@gmail.com>
2024-07-15 21:07:49 +0700

    Updates
    + Update CSS
    + Update Feedback Form

--- src/views/SessionFeedback.js
+++ src/views/SessionFeedback.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { CCard, CCardBody, CSpinner } from '@coreui/react'
+import { CCard, CCardBody, CSpinner, CAlert } from '@coreui/react'
+import styles from '../assets/css/styles.module.css'
 
 const SessionFeedback = () => {
   const [isLoading, setIsLoading] = useState(true)
@@ -10,24 +11,16 @@ const SessionFeedback = () => {
 
   return (
     <CCard className={'border-info'} style={{ height: '100vh' }}>
-      <CCardBody style={{ height: '100%', padding: 0, position: 'relative' }}>
+      <CCardBody className={styles.cardbody}>
+        <CAlert color="info" variant='solid' dismissible style={{ margin: 0 }}>
+          This form is hosted by Google Forms. You need to accept cookies to view and submit your feedback.
+        </CAlert>
         {isLoading && (
-          <div style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: 'rgba(255, 255, 255, 0.8)'
-          }}>
+          <div className={styles.spinner}>
             <CSpinner color="info" />
           </div>
         )}
         <iframe
-          sandbox="allow-scripts"
           src="https://docs.google.com/forms/d/e/1FAIpQLScdJ3888X_Z-c7n72WfCDAqMEWDWZxF_K_PFLXeRcSTy7aH5w/viewform?embedded=true"
           width="100%"
           height="100%"
@@ -35,7 +28,7 @@ const SessionFeedback = () => {
           title="Session Feedback Form"
           onLoad={handleIframeLoad}
         >
-          Loading…
+          Loading...
         </iframe>
       </CCardBody>
     </CCard>
