b4a4fcc75826f8efb25e66f17e8d2bc9e3a6a6c6
Nguyễn Phúc Khang <khang2073@gmail.com>
2024-07-18 00:52:38 +0700

    Updates
    + Update CSS styling for footer
    + Add footer logo

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -1,28 +1,27 @@
 import React from 'react'
-import { CFooter } from '@coreui/react'
+import { CFooter, CImage } from '@coreui/react'
 import styles from '../assets/css/styles.module.css'
+import aiot from '../assets/images/AIoT.png'
 
 const AppFooter = () => {
   return (
     <CFooter className={styles.footer}>
       <div>
-        <a className={styles.hreflink} href="https://bme.hcmiu.edu.vn/bme10/" target="_blank" rel="noopener noreferrer">
-          BME10
-        </a>
-        <span className="ms-1">
-          &copy; School of Biomedical Engineering International University, Vietnam National
-          Universities, HCMC
-        </span>
-      </div>
-      <div className="ms-auto">
+        BME10 &copy; School of Biomedical Engineering International University, Vietnam National
+        Universities, HCMC
+        <div style={{marginTop: "10px"}}>
         <span className="me-1">
-        Designed by Nguyen Phuc Khang - {' '}
+        <CImage fluid src={aiot} className={styles.footerlogo} />
+          Designed by Nguyen Phuc Khang - {' '}
           <a className={styles.hreflink} href="https://aiotlab.vn/" target="_blank" rel="noopener noreferrer">
             AIoT Lab VN
           </a>
         </span>
+        </div>
       </div>
+      
     </CFooter>
+
   )
 }
 
