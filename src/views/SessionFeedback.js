00dc3f45db6e4d00d1b27e267326ecaf66aa4e46
Nguyễn Phúc Khang <khang2073@gmail.com>
2024-07-24 10:53:46 +0700

    Update form link

--- src/views/SessionFeedback.js
+++ src/views/SessionFeedback.js
@@ -21,7 +21,7 @@ const SessionFeedback = () => {
           </div>
         )}
         <iframe
-          src="https://docs.google.com/forms/d/e/1FAIpQLScdJ3888X_Z-c7n72WfCDAqMEWDWZxF_K_PFLXeRcSTy7aH5w/viewform?embedded=true"
+          src="https://docs.google.com/forms/d/e/1FAIpQLSd1utSpswFcVEYYgRM0CAPwIQX6yYdUQ-kB6vQJzdn-RD2qZg/viewform?embedded=true"
           width="100%"
           height="85%"
           style={{ border: 'none' }}
